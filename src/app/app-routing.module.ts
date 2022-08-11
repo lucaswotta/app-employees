dd9489b5b322c048612f2ccc0c62387e6ec5134a
lucaswotta <lucaswotta@gmail.com>
2022-08-11 18:54:38 -0300

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TableEmployeesComponent } from './pages/table-employees/table-employees.component';
+import { AddEmployeComponent } from './pages/add-employe/add-employe.component';
+import { EditEmployeComponent } from './pages/edit-employe/edit-employe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home"
+  },
+  {
+    path: "home",
+    component: TableEmployeesComponent
+  },
+  {
+    path: "new",
+    component: AddEmployeComponent
+  },
+  {
+    path: "edit/:id",
+    component: EditEmployeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAJES-PER5cD_07vA91kV8vWU-m0sq5rds",
+    authDomain: "app-employess.firebaseapp.com",
+    projectId: "app-employess",
+    storageBucket: "app-employess.appspot.com",
+    messagingSenderId: "178960595772",
+    appId: "1:178960595772:web:5574e05e737c2bb86cfc39"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
